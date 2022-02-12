52b0fdfa2e2a6a9a88bc7237b2ffa127cf3698d5
hmdne <54514036+hmdne@users.noreply.github.com>
2022-02-12 05:38:37 +0100

    Tests: Improve the playground spec

--- spec/playground_spec.rb
+++ spec/playground_spec.rb
@@ -5,35 +5,57 @@ RSpec.describe "Playground", type: :feature, js: true do
     visit "/playground"
   end
 
-  it "should be able to evaluate simple Ruby code" do
-    set_code <<~RUBY
-      def test
-        2
-      end
-
-      test + test * test
-    RUBY
-
-    click_button "Run"
-
-    code(:output).chomp.should == "6"  
+  playground_test = -> msg, result, ruby do
+    it msg do
+      set_code ruby
+      click_button "Run"
+      code(:output).chomp.should be == result
+    end
   end
 
-  it "should be able to evaluate more advanced Ruby code" do
-    set_code <<~RUBY
-      (1..100).each do |i|
-        if (i == 50) .. (i == 55)
-          puts i
-        end
+  playground_test.call "should be able to evaluate simple Ruby code", "6", <<~RUBY
+    def test
+      2
+    end
+
+    test + test * test
+  RUBY
+
+  playground_test.call "should support flip-flops", (50..55).to_a.join("\n"), <<~RUBY
+    (1..100).each do |i|
+      if (i == 50) .. (i == 55)
+        puts i
       end
+    end
+  RUBY
 
-      {a: 6, b: 7} => {a:}
+  playground_test.call "should support pattern matching", "6", <<~RUBY
+    {a: 6, b: 7} => {a:}
+    puts a
+  RUBY
 
-      puts a
-    RUBY
+  playground_test.call "should support classes, modules and refinements", "123", <<~RUBY
+    module M
+      def m = 3
+    end
 
-    click_button "Run"
+    class A
+      include M
+      def m = super + 20
+    end
 
-    code(:output).chomp.should == [50,51,52,53,54,55,6].join("\n")
-  end
+    class B < A
+      def m = super + 50
+    end
+
+    module R
+      refine B do
+        def m = super + 50
+      end
+    end
+
+    using R
+
+    B.new.m
+  RUBY
 end
