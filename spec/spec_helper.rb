6719895937543a8b84ed5a91f70d3193270d7f90
hmdne <54514036+hmdne@users.noreply.github.com>
2023-01-05 12:07:58 +0100

    Fix the problem with specs timeouting on CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,13 @@
 # in prefork mode. Let's investigate it later.
 ENV['OPAL_PREFORK_DISABLE'] = '1'
 
+require 'middleman/rack'
+require "capybara/rspec"
+require "capybara/cuprite"
+
+# Prefetch the first load, so that we don't timeout
+Middleman.server.call("PATH_INFO" => "/javascripts/application.js", "SCRIPT_NAME" => "/")
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
@@ -17,26 +24,23 @@ RSpec.configure do |config|
   config.default_formatter = "doc"
 
   config.expect_with(:rspec) { |c| c.syntax = :should }
-end
 
-require "capybara/rspec"
-require "capybara/cuprite"
-Capybara.javascript_driver = :cuprite
-Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app,
-    window_size: [1200, 800],
-    browser_options: { 'no-sandbox': nil },
-    inspector: ENV['INSPECTOR'],
-    headless: !ENV['NO_HEADLESS'],
-    timeout: 20,
-    url_blacklist: [
-      "https://docs.ruby-lang.org/", # It is quite a heavy website iframed on playground
-                                     # and for testing, we don't need it to be present.
-                                     # This should spare us from random test failures.
-    ],
-  )
-end
+  Capybara.javascript_driver = :cuprite
+  Capybara.register_driver(:cuprite) do |app|
+    Capybara::Cuprite::Driver.new(app,
+      window_size: [1200, 800],
+      browser_options: { 'no-sandbox': nil },
+      inspector: ENV['INSPECTOR'],
+      headless: !ENV['NO_HEADLESS'],
+      timeout: 20,
+      url_blacklist: [
+        "https://docs.ruby-lang.org/", # It is quite a heavy website iframed on playground
+                                      # and for testing, we don't need it to be present.
+                                      # This should spare us from random test failures.
+      ],
+    )
+  end
 
-require 'middleman/rack'
-Capybara.app = Middleman.server
-Capybara.server = :puma
+  Capybara.app = Middleman.server
+  Capybara.server = :puma
+end
