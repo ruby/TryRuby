5f04dc5de1141b2fb9f797a18b3104c72cf64df3
Benoit Daloze <eregontp@gmail.com>
2023-07-28 15:36:04 +0200

    List CRuby first since it is the default now
    * That fixes the dropdown order at https://try.ruby-lang.org/playground/
      to have the default first.

--- app/ruby_engine.rb
+++ app/ruby_engine.rb
@@ -29,8 +29,6 @@ class RubyEngine
   # When you update the engines, ensure that they are tested correctly.
   # Update the engine list also in spec/playground_spec.
   ENGINES = [
-    # Opal.new,
-    OpalWebWorker.new("1.7.1"),
     CRubyWASI.new(
       "https://cdn.jsdelivr.net/npm/ruby-head-wasm-wasi@0.5.0-2022-12-25-a/dist/ruby.wasm",
       "3.2.0"
@@ -41,6 +39,9 @@ class RubyEngine
     #   "https://cdn.jsdelivr.net/npm/ruby-head-wasm-wasi@next/dist/ruby.wasm",
     #   "3.3.0dev"
     # ),
+
+    # Opal.new,
+    OpalWebWorker.new("1.7.1"),
   ].each_with_object({}) do |engine, hash|
     hash[engine.engine_id] = engine
   end
